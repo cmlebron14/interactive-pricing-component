5c6c6922f1cf89160bffb3218f544f8f7aac782f
Christina Lebron <cmlebron14@gmail.com>
2021-05-06 23:05:09 -0400

    functionality round 1

---
+++ scripts.js
@@ -0,0 +1,43 @@
+/*
+ * [0] 10K pageviews / $8 per month
+ * [1] 50K pageviews / $12 per month
+ * [2] 100K pageviews / $16 per month
+ * [3] 500k pageviews / $24 per month
+ * [4] 1M pageviews / $36 per month
+ */
+
+/*
+ * 1. on page load, the default value of the slider should be [2] 100K pageviews / $16 per month
+ * 2. when the user drags the slider:
+ *      - the pageview value and cost value should update
+ *      - the slider bg linear gradient should update
+ * 3. on page load, the default value of the toggle should be monthly
+ * 4. when the user changes to yearly:
+ *      - the 25% discount should be applied to all prices
+ */
+
+const slider = document.querySelector('.slider');
+const sliderProgressBar = document.querySelector('#track');
+console.log(sliderProgressBar);
+const pageviews = document.querySelector('.pageview-value');
+const price = document.querySelector('.price-value');
+
+const pageviewValues = ['10K', '50K', '100K', '500K', '1M'];
+const priceValues = ['8', '12', '16', '24', '36'];
+const sliderGradients = [
+  'linear-gradient(to right, #ecf0fb 0%, #ecf0fb 100%)',
+  'linear-gradient(to right, #a4f3eb 0%, #a4f3eb 25%, #ecf0fb 25%, #ecf0fb 100%)',
+  'linear-gradient(to right, #a4f3eb 0%, #a4f3eb 50%, #ecf0fb 50%, #ecf0fb 100%)',
+  'linear-gradient(to right, #a4f3eb 0%, #a4f3eb 75%, #ecf0fb 75%, #ecf0fb 100%)',
+  'linear-gradient(to right, #ecf0fb 0%, #ecf0fb 100%)'
+];
+
+slider.addEventListener('input', e => {
+  pageviews.textContent = pageviewValues[slider.value];
+  price.textContent = priceValues[slider.value];
+  // sliderProgressBar.style.background = sliderGradients[slider.value];
+});
+
+//console.log(pageviewValueSpan.textContent);
+//console.log(priceValueSpan.textContent);
+//console.log(slider.value);
