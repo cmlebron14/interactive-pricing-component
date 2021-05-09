e0d54a5aec86fb3966638347475bb87f479ab717
Christina Lebron <cmlebron14@gmail.com>
2021-05-08 21:51:06 -0400

    refactor styles and make responsive

--- scripts.js
+++ scripts.js
@@ -1,24 +1,6 @@
-/*
- * [0] 10K pageviews / $8 per month
- * [1] 50K pageviews / $12 per month
- * [2] 100K pageviews / $16 per month
- * [3] 500k pageviews / $24 per month
- * [4] 1M pageviews / $36 per month
- */
-
-/*
- * 1. on page load, the default value of the slider should be [2] 100K pageviews / $16 per month
- * 2. when the user drags the slider:
- *      - the pageview value and cost value should update
- *      - the slider bg linear gradient should update
- * 3. on page load, the default value of the toggle should be monthly
- * 4. when the user changes to yearly:
- *      - the 25% discount should be applied to all prices
- */
-
 const slider = document.querySelector('.slider');
 const sliderProgressBar = document.querySelector('.slider-bg');
-const pageviews = document.querySelector('.pageview-value');
+const pageviews = document.querySelector('.pageviews-value');
 const price = document.querySelector('.price-value');
 
 const yearlyPricing = document.querySelector('#yearly');
